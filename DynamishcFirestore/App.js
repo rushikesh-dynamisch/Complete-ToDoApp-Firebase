d694fb3cf6a780d367f5755a701d65d8f0ffc72a
rushikesh-dynamish <rushikesh.dabhade@dynamisch.co>
2022-12-21 18:46:30 +0530

    Added onRefresh and pagination

--- DynamishcFirestore/App.js
+++ DynamishcFirestore/App.js
@@ -1,76 +1,13 @@
-// import React from "react";
-// import { View,Text,StyleSheet, Touchable, TouchableOpacity } from "react-native";
-// import firestore from '@react-native-firebase/firestore';
-// import { Todo } from "./Todo";
-// import TaskItem from "./component/TaskItem";
-// const App= () => {
-//   const GetData=()=>
-//   {
-//   firestore()
-//   .collection('users')
-//   .doc('mydatabase')
-//   .get()
-//   .then(documentSnapshot => {
-//     console.log('User exists: ', documentSnapshot.exists);
-
-//     if (documentSnapshot.exists) {
-//       console.log('User data: ', documentSnapshot.data());
-//       const data = documentSnapshot.data();
-//       console.log("New data",data.name)
-//     }
-//   });
-// }
-
-//   // return (
-//   //   <View>
-     
-//   //     <TouchableOpacity style={styles.getbtn} onPress={GetData}>
-//   //       <Text style={styles.gettxt}>GetData</Text>
-//   //     </TouchableOpacity>
-//   //     <Todo/>
-//   //   </View>
-//   // )
 
 
-//   return (
-//     <View>
-//       <Todo/>
-//     </View>
-//   )
 
-
-// };
-
-// const styles = StyleSheet.create({
-//   sectionContainer: {
-//     marginTop: 32,
-//     paddingHorizontal: 24,
-//   },
-//   getbtn:{
-  
-//     backgroundColor:'#008CBA',
-//     width:100,
-//     height:30,
-//     marginLeft:130,
-//     marginTop:50,
-//     display:'flex',
-    
-//   },
-//   gettxt:{
-//     marginLeft:20,
-//     marginTop:5,
-//     color:'white'
-//   }
-// });
-// export default App;
-
-import React, {useState,useEffect} from 'react';
-import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, Alert, FlatList,RefreshControl } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, ScrollView, Alert, FlatList, RefreshControl } from 'react-native';
 import { Task2 } from './component/Task2';
 import firestore from '@react-native-firebase/firestore';
 import { GoogleIn } from './GoogleIn';
-export default function App() 
-{
+import { Button } from 'react-native-paper';
+export default function App() {
 
   const wait = (timeout) => {
     return new Promise(resolve => setTimeout(resolve, timeout));
@@ -80,168 +17,202 @@ export default function App()
 
   const onRefresh = React.useCallback(() => {
     Alert.alert('resfreshing the screen.');
+    fetchMoredata();
+
+
     setRefreshing(true);
     wait(1000).then(() => setRefreshing(false));
   }, []);
 
 
 
-  let newarr =[];
- const[arr,setarr]=useState([]);
+  let newarr = [];
+  const [arr, setarr] = useState([]);
   const [task, setTask] = useState();
   const [taskItems, setTaskItems] = useState([]);
+  const [lastDocument, setLastDocument] = useState();
+  const [userData, setUserData] = useState([]);
+  const [post,setpost]=useState([]);
+  const [postsPerLoad]=useState(10);
+  const [StartAfter,setStartAfter]=useState({});
   const ref = firestore().collection('LatestDocument');
-  
+ 
   useEffect(() => {
-    const GetData=()=>
-      {
-      // firestore()
-      // .collection('LatestDocument')
-      // .get()
-      // .then(documentSnapshot => {
-      //   console.log('User exists1: ', documentSnapshot.exists);
-    
-      //   if (documentSnapshot.exists) {
-      //     console.log('User data1: ', documentSnapshot.data());
-      //     const data = documentSnapshot.data();
-      //     console.log("New data1",data.name)
-      //     setarr([data.name]);
-      //     console.log('new array1',arr);
-      //   }
-      // });
-      firestore()
-  .collection('LatestDocument')
-  .get()
-  .then(querySnapshot => {
-    console.log('Total users: ', querySnapshot.size);
-
-    querySnapshot.forEach(documentSnapshot => {
-      console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
-      let documentdata = documentSnapshot.data();
-      //  setarr(documentdata.name);
-       newarr.push(documentdata.name);
-       console.log('newarr',newarr)
-       setarr(newarr);
-    });
-    
-  });
-      }
-    GetData();
+   
+    fetchdata();
   }, []);
+
+async function fetchdata()
+{
+  const postData = await GetData(postsPerLoad);
+  setpost([...arr, ...postData.arr]);
+  setStartAfter(postData.lastvisible);
+}
+
+async function fetchMoredata()
+{
+  const postData = await  Getmoreposts(StartAfter,postsPerLoad);
+  setpost([...arr, ...postData.arr]);
+  setStartAfter(postData.lastvisible);
+}
+
+
+  const GetData = async (postsPerLoad) => {
+  let lastvisible;
+   const snapshot=await firestore()
+      .collection('LatestDocument')
+      .orderBy('name','desc')
+      .limit(postsPerLoad)
+      .get()
+      .then(querySnapshot => {
+        console.log('Total users: ', querySnapshot.size);
+        let lastvisible =querySnapshot.docs[querySnapshot.docs.length-1];
+        console.log("lastitem",lastvisible);
+        querySnapshot.forEach(documentSnapshot => {
+          console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+          let documentdata = documentSnapshot.data();
+          //  setarr(documentdata.name);
+          newarr.push(documentdata.name);
+          console.log('newarr', newarr)
+          setarr(newarr);
+        });
+       
+      });
+      return {arr,lastvisible}
+  }
+
 
   const handleAddTask = () => {
     Keyboard.dismiss();
     setTaskItems([...taskItems, task])
     async function addTodo() {
       ref.doc(task)
-      .set({
-        name: task,
-      })
-      .then(() => {
-        console.log('User added!');
-      });
-    
+        .set({
+          name: task,
+        })
+        .then(() => {
+          console.log('User added!');
+        });
+
     }
 
     addTodo();
-    
+
   }
 
-  const DeleteTask=(deleteitem)=>
-  {
-    
+  const DeleteTask = (deleteitem) => {
+
     firestore()
-    .collection('LatestDocument')
-    .doc(deleteitem)
-    .delete()
-    .then(() => {
-      console.log('User deleted!');
-    });
+      .collection('LatestDocument')
+      .doc(deleteitem)
+      .delete()
+      .then(() => {
+        console.log('User deleted!');
+      });
     Alert.alert('Deleted');
   }
 
   const completeTask = (item) => {
-    
-    // let itemsCopy = [...taskItems];
-    // itemsCopy.splice(index, 1);
-    // setTaskItems(itemsCopy)
-    async function updateTodo()
-    {
-    ref.doc('item1')
-  .update({
-    name: 'latestupdate',
-  })
-  .then(() => {
-    console.log('User updated!');
-  });
+    async function updateTodo() {
+      ref.doc('item1')
+        .update({
+          name: 'latestupdate',
+        })
+        .then(() => {
+          console.log('User updated!');
+        });
     }
-    
+
     updateTodo();
   }
-  // const Taskset=(task)=>
-  // {
-  //  setTask(task);
-  // }
-  
-  const UpdateTask=(UpdateItem)=>
-  {
+  const UpdateTask = (UpdateItem) => {
     console.log('update button');
     firestore()
-  .collection('LatestDocument')
-  .doc(UpdateItem)
-  .update({
-    name:task,
-  })
-  .then(() => {
-    console.log('User updated!');
-  });
-  setTask(null);
-  Alert.alert('Updated');
+      .collection('LatestDocument')
+      .doc(UpdateItem)
+      .update({
+        name: task,
+      })
+      .then(() => {
+        console.log('User updated!');
+      });
+    setTask(null);
+    Alert.alert('Updated');
   }
-  return (
+  function MakeUserData(docs) {
+    let templist = [];
+    docs.forEach((doc, i) => {
+      console.log(doc._data);
+      let temp = (
+        <View key={i} style={{ margin: 10 }}>
+          <Text>{doc._data.name}</Text>
+          <Text>{doc._data.age}</Text>
+        </View>
+      );
+      templist.push(temp);
+    });
+    setUserData(templist);
+  }
+
+  const Getmoreposts=async(StartAfter,postsPerLoad)=>
+  {
+ const snapshot=await firestore()
+   .collection('LatestDocument')
+   .orderBy('name','desc')
+   .startAfter(StartAfter)
+   .limit(postsPerLoad)
+   .get()
+   .then(querySnapshot => {
+     console.log('Total users: ', querySnapshot.size);
+     let lastvisible =querySnapshot.docs[querySnapshot.docs.length-1];
+     console.log("lastitem",lastvisible);
+     querySnapshot.forEach(documentSnapshot => {
+       console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+       let documentdata = documentSnapshot.data();
+       //  setarr(documentdata.name);
+       newarr.push(documentdata.name);
+       console.log('newarr', newarr)
+       setarr(newarr);
+     });
     
+   });
+   return {arr,lastvisible}
+ }
+
+  return (
+
     <View style={styles.container}>
-      {/* Added this scroll view to enable scrolling when list gets longer than the page */}
-     <GoogleIn/>
-      
+      {/* <TouchableOpacity style={styles.Paging} onPress={() => LoadData()}><Text style={styles.deltext}>ClikButton</Text></TouchableOpacity> */}
+      {/* <GoogleIn/> */}
+
       {/* Today's Tasks */}
       <View style={styles.tasksWrapper}>
         <Text style={styles.sectionTitle}>Today's tasks</Text>
         <View style={styles.items}>
-      <FlatList
-        refreshControl={
-      <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-       }
-     
-    
-      data={arr}
-      renderItem={({ item }) => (
-        
-        // <TouchableOpacity  onPress={() => DeleteTask(item)}>
-        // <TouchableOpacity>
-        // <Task2 text={item} /> 
-        // </TouchableOpacity>
+          <FlatList
+           vertical
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
+           
 
-
-        <TouchableOpacity>
-        <TouchableOpacity onPress={()=>setTask(item)}>
-        <Text style={styles.textstyle}>{item}</Text>
-        </TouchableOpacity>
-        <TouchableOpacity  onPress={()=>DeleteTask(item)} style={styles.deletebtn}><Text style={styles.deltext}>DELETE</Text></TouchableOpacity>
-        <TouchableOpacity onPress={()=>UpdateTask(item)} style={styles.updatebtn}><Text style={styles.upltext}>UPDATE</Text></TouchableOpacity>
-        </TouchableOpacity>
-      
-      )}
-      
-    />
-
+            data={arr}
+            renderItem={({ item }) => (
+              <TouchableOpacity>
+                <TouchableOpacity onPress={() => setTask(item)}>
+                  <Text style={styles.textstyle}>{item}</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={() => DeleteTask(item)} style={styles.deletebtn}><Text style={styles.deltext}>DELETE</Text></TouchableOpacity>
+                <TouchableOpacity onPress={() => UpdateTask(item)} style={styles.updatebtn}><Text style={styles.upltext}>UPDATE</Text></TouchableOpacity>
+              </TouchableOpacity>
+            )}
+            // onEndReached={Getmoreposts}
+            // onEndReachedThreshold ={0.01}
+            // scrollEventThrottle ={150}
+          />
         </View>
       </View>
-    
-
-      {/* Write a task */}
-      {/* Uses a keyboard avoiding view which ensures the keyboard does not cover the items on screen */}
-      <KeyboardAvoidingView 
+      <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
         style={styles.writeTaskWrapper}
       >
@@ -252,7 +223,6 @@ export default function App()
           </View>
         </TouchableOpacity>
       </KeyboardAvoidingView>
-      
     </View>
   );
 }
@@ -269,7 +239,7 @@ const styles = StyleSheet.create({
   sectionTitle: {
     fontSize: 24,
     fontWeight: 'bold',
-    color:'white'
+    color: 'white'
   },
   items: {
     marginTop: 30,
@@ -280,7 +250,8 @@ const styles = StyleSheet.create({
     width: '100%',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    alignItems: 'center'
+    alignItems: 'center',
+  
   },
   input: {
     paddingVertical: 15,
@@ -290,6 +261,7 @@ const styles = StyleSheet.create({
     borderColor: '#C0C0C0',
     borderWidth: 1,
     width: 250,
+    // marginTop:100,
   },
   addWrapper: {
     width: 60,
@@ -300,6 +272,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     borderColor: '#C0C0C0',
     borderWidth: 1,
+    // marginTop:900,
   },
   circular2: {
     width: 20,
@@ -307,46 +280,54 @@ const styles = StyleSheet.create({
     borderColor: '#55BCF6',
     borderWidth: 2,
     borderRadius: 5,
-    marginLeft:-140
+    marginLeft: -140,
+   
   },
-  textstyle:{
-  color:'white',
-  margin:3,
-  padding:3,
-  fontSize:16
+  textstyle: {
+    color: 'white',
+    margin: 3,
+    padding: 3,
+    fontSize: 16
   },
-  deletebtn:{
-  width:60,
- height:25,
- backgroundColor:'red',
- marginLeft:150,
- marginTop:-25,
- borderWidth:1,
- borderColor:'white'
+  deletebtn: {
+    width: 60,
+    height: 25,
+    backgroundColor: 'red',
+    marginLeft: 150,
+    marginTop: -25,
+    borderWidth: 1,
+    borderColor: 'white'
   },
-  updatebtn:{
-    width:60,
-   height:25,
-   backgroundColor:'red',
-   marginLeft:220,
-   marginTop:-25,
-   borderWidth:1,
-   borderColor:'white'
-    },
-    deltext:
-    {
-      color:'white',
-      marginLeft:5
-    },
-    upltext:
-    {
-      color:'white',
-      margin:'auto',
-      marginLeft:2
-    },
+  updatebtn: {
+    width: 60,
+    height: 25,
+    backgroundColor: 'red',
+    marginLeft: 220,
+    marginTop: -25,
+    borderWidth: 1,
+    borderColor: 'white'
+  },
+  deltext:
+  {
+    color: 'white',
+    marginLeft: 5
+  },
+  upltext:
+  {
+    color: 'white',
+    margin: 'auto',
+    marginLeft: 2
+  },
+  Paging: {
+    width: 60,
+    height: 25,
+    backgroundColor: 'red',
+    marginLeft: 220,
+
+    borderWidth: 1,
+    borderColor: 'white'
+  },
   addText: {},
-}); 
-
-
+});
 
 
